73a1bde7c3670402ee12b9fb83c0172439750ded
Mario Dickerson <dai.dickerson@gmail.com>
2024-05-09 19:52:03 -0500

    made changes to help expand features of app

--- src/main/java/com/Bingo/Bingo/controllers/CardController.java
+++ src/main/java/com/Bingo/Bingo/controllers/CardController.java
@@ -5,36 +5,45 @@ import com.Bingo.Bingo.models.OrderedOptions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 
 @Controller
+@RequestMapping("bingo")
 public class CardController {
 
     ListData listData = new ListData();
 
     @GetMapping("create")
-    public String createBingoCards(Model model) {
-        return "create";
+    public String createBingoCards (Model model) {
+
+        return "bingo/create";
     }
 
 
-    @GetMapping("/")
-    public String bingoCards(Model model) {
+//    @GetMapping
+//    public String bingoCards(Model model) {
+//
+//        OrderedOptions bingo1 = new OrderedOptions(listData.getArray(0));
+//        //OrderedOptions bingo2 = new OrderedOptions(selection);
+//        //OrderedOptions bingo3 = new OrderedOptions(selection);
+//
+//        ArrayList<OrderedOptions> bingo = new ArrayList<>();
+//        bingo.add(bingo1);
+//        //bingo.add(bingo2);
+//        //bingo.add(bingo3);
+//
+//        model.addAttribute("bingoList", bingo);
+//
+//
+//        return "index";
+//    }
 
-        OrderedOptions bingo1 = new OrderedOptions(listData.getArray(0));
-        //OrderedOptions bingo2 = new OrderedOptions(selection);
-        //OrderedOptions bingo3 = new OrderedOptions(selection);
+    @GetMapping("Edit")
+    public String editBingoCard (Model model) {
 
-        ArrayList<OrderedOptions> bingo = new ArrayList<>();
-        bingo.add(bingo1);
-        //bingo.add(bingo2);
-        //bingo.add(bingo3);
-
-        model.addAttribute("bingoList", bingo);
-
-
-        return "index";
+        return "bingo/Edit";
     }
 
 }
---
+++ src/main/java/com/Bingo/Bingo/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.Bingo.Bingo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index(Model model) {
+
+        return "index";
+    }
+
+}
