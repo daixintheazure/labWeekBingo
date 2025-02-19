8a2f6d0fe0d2f74bea5a6e99d7aff8518a102ea7
Mario Dickerson <dai.dickerson@gmail.com>
2025-02-19 17:17:34 -0600

    Created BingoOption object and linked to DB

---
+++ src/main/java/com/Bingo/Bingo/models/BingoOption.java
@@ -0,0 +1,61 @@
+package com.Bingo.Bingo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Objects;
+
+@Entity
+public class BingoOption {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @NotBlank
+    private String bingoOption;
+
+    public BingoOption() {}
+
+    public BingoOption(String option) {
+        this.bingoOption = option;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BingoOption that = (BingoOption) o;
+        return id == that.id && Objects.equals(bingoOption, that.bingoOption);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, bingoOption);
+    }
+
+    @Override
+    public String toString() {
+        return bingoOption;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBingoOption() {
+        return bingoOption;
+    }
+
+    public void setBingoOption(String bingoOption) {
+        this.bingoOption = bingoOption;
+    }
+}
